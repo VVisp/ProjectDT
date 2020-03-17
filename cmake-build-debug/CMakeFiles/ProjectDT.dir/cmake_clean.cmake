175b6c8da499c9b8020cc30eec02213377c6ee20
Stan Brunau <44241775+VVisp@users.noreply.github.com>
2020-03-17 18:57:54 +0100

    Added Initial Project Files

---
+++ cmake-build-debug/CMakeFiles/ProjectDT.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjectDT.dir/main.cpp.o"
+  "ProjectDT"
+  "ProjectDT.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjectDT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
