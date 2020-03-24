bb21be6fb0d66ebbf20731bfa22d5e0ada608403
Stan <44241775+VVisp@users.noreply.github.com>
2020-03-24 19:29:28 +0100

    Begin Implementatie JSON bestand inlezen

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <fstream>
 #include "tree.h"
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ifstream json;
+    json.open("tree.json");
+
+    if (!json.is_open()) {
+        return 1;
+    }
+
+    while (json) {
+        string line;
+        getline(json, line, ':');
+
+        string leaf;
+        json >> leaf;
+
+        leaf.erase(leaf.begin());
+
+        if (leaf == "{") {
+            continue;
+        }
+
+        cout << leaf << endl;
+    }
+
+    json.close();
+
     return 0;
 }
